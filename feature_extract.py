cd576bc6cc067b7af1bf5be418e0e4bbc0cc2ba9
Vishnu Dasu <vishnu98dasu@gmail.com>
2017-07-30 12:38:45 +0530

    Update feature_extract

--- feature_extract.py
+++ feature_extract.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import glob
+import re
 
 for image in glob.glob('*.jpg'):
     I = cv2.imread(image)
@@ -10,6 +11,15 @@ for image in glob.glob('*.jpg'):
     g_y = cv2.Sobel(I_s,cv2.CV_32F,0,1,ksize=1)
 
     g, arg = cv2.cartToPolar(g_x,g_y,angleInDegrees = True)"""
+    X = np.array([])
+    y = []
     
     hog = cv2.HOGDescriptor((128,128),(32,32),(16,16),(16,16),9)
-    descriptor = hog.compute(I)
+    X = np.append(X,hog.compute(I))
+    m = re.search('_(\w+)',image)
+    if m.group(1)=='pos':
+        y.append(1)
+    elif m.group(1)=='neg':
+        y.append(0)
+
+    print (X)
